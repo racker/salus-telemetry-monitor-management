ac236462c280fcb5b89ecf8e21c481bec2a1ae43
George Jahad <github@blackbirdsystems.net>
2019-02-08 16:02:54 -0800

    created

---
+++ src/main/java/com/rackspace/salus/monitor_management/SalusTelemetryMonitorManagementApplication.java
@@ -0,0 +1,14 @@
+package com.rackspace.salus.monitor_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalusTelemetryMonitorManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalusTelemetryMonitorManagementApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/rackspace/salus/monitor_management/SalusTelemetryMonitorManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rackspace.salus.monitor_management;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalusTelemetryMonitorManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
