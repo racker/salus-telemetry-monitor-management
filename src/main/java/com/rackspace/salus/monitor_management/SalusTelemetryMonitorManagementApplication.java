b0e19c8e4143bc4f5e7850f442c3cbc4ebca5b61
George Jahad <github@blackbirdsystems.net>
2019-02-08 18:37:55 -0800

    creating

--- src/main/java/com/rackspace/salus/monitor_management/SalusTelemetryMonitorManagementApplication.java
+++ src/main/java/com/rackspace/salus/monitor_management/SalusTelemetryMonitorManagementApplication.java
@@ -2,8 +2,12 @@ package com.rackspace.salus.monitor_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.rackspace.salus.telemetry.repositories")
+@EntityScan("com.rackspace.salus.telemetry.model")
 public class SalusTelemetryMonitorManagementApplication {
 
     public static void main(String[] args) {
