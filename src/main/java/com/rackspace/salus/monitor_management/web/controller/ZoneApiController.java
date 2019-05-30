d7222d571f8a5b12aa21cbb8fbd34a8b6c364185
James Buchan <james.buchan@rackspace.com>
2019-05-30 13:33:21 -0500

    Handle public zones in GET zone endpoint (#37)
    * Handle public zones in GET zone endpoint
    
    * Add missing view annotations
    
    * Fix tests
    
    * Small refactor and indentation

--- src/main/java/com/rackspace/salus/monitor_management/web/controller/ZoneApiController.java
+++ src/main/java/com/rackspace/salus/monitor_management/web/controller/ZoneApiController.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.rackspace.salus.monitor_management.entities.Monitor;
 import com.rackspace.salus.telemetry.model.View;
 import com.rackspace.salus.monitor_management.web.model.ZoneCreatePublic;
+import com.rackspace.salus.telemetry.etcd.types.ResolvedZone;
 import com.rackspace.salus.telemetry.model.NotFoundException;
 import com.rackspace.salus.monitor_management.errors.ZoneAlreadyExists;
 import com.rackspace.salus.monitor_management.entities.Zone;
@@ -35,9 +36,11 @@ import io.swagger.annotations.AuthorizationScope;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,6 +50,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 
 @Slf4j
 @RestController
@@ -60,96 +64,123 @@ import org.springframework.web.bind.annotation.RestController;
         })
 })
 public class ZoneApiController implements ZoneApi {
-    private ZoneManagement zoneManagement;
+  private ZoneManagement zoneManagement;
 
-    public ZoneApiController(ZoneManagement zoneManagement) {
-        this.zoneManagement = zoneManagement;
-    }
+  public ZoneApiController(ZoneManagement zoneManagement) {
+    this.zoneManagement = zoneManagement;
+  }
 
-    @Override
-    @GetMapping("/tenant/{tenantId}/zones/{name}")
-    @ApiOperation(value = "Gets specific zone by tenant id and zone name")
-    @JsonView(View.Public.class)
-    public ZoneDTO getByZoneName(@PathVariable String tenantId, @PathVariable String name) {
-        Optional<Zone> zone = zoneManagement.getPrivateZone(tenantId, name);
-        return zone.orElseThrow(() -> new NotFoundException(String.format("No zone found named %s on tenant %s",
-                name, tenantId)))
-                .toDTO();
-    }
+  @GetMapping("/tenant/{tenantId}/zones/**")
+  @ApiOperation(value = "Gets specific zone by tenant id and zone name")
+  @JsonView(View.Public.class)
+  public ZoneDTO getAvailableZone(@PathVariable String tenantId, HttpServletRequest request) {
+    String name = extractZoneNameFromUri(request);
+    return getByZoneName(tenantId, name);
+  }
 
-    @GetMapping("/admin/zones/{name}")
-    @ApiOperation(value = "Gets specific public zone by name")
-    @JsonView(View.Admin.class)
-    public ZoneDTO getPublicZone(@PathVariable String name) {
-        Optional<Zone> zone = zoneManagement.getPublicZone(name);
-        return zone.orElseThrow(() -> new NotFoundException(String.format("No public zone found named %s",
-            name)))
-            .toDTO();
+  @Override
+  public ZoneDTO getByZoneName(String tenantId, String name) {
+    Optional<Zone> zone;
+    if (name.startsWith(ResolvedZone.PUBLIC_PREFIX)) {
+      zone = zoneManagement.getPublicZone(name);
+    } else {
+      zone = zoneManagement.getPrivateZone(tenantId, name);
     }
+    return zone
+        .orElseThrow(() -> new NotFoundException(String.format("No zone found named %s", name)))
+        .toDTO();
+  }
 
-    @PostMapping("/tenant/{tenantId}/zones")
-    @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "Creates a new private zone for the tenant")
-    @JsonView(View.Public.class)
-    public ZoneDTO create(@PathVariable String tenantId, @Valid @RequestBody ZoneCreatePrivate zone)
-            throws ZoneAlreadyExists {
-        return zoneManagement.createPrivateZone(tenantId, zone).toDTO();
-    }
+  @GetMapping("/admin/zones/**")
+  @ApiOperation(value = "Gets specific public zone by name")
+  @JsonView(View.Admin.class)
+  public ZoneDTO getPublicZone(HttpServletRequest request) {
+    String name = extractZoneNameFromUri(request);
+    return getByZoneName(null, name);
+  }
 
-    @PostMapping("/admin/zones")
-    @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "Creates a new public zone")
-    public ZoneDTO create(@Valid @RequestBody ZoneCreatePublic zone)
-        throws ZoneAlreadyExists {
-        return zoneManagement.createPublicZone(zone).toDTO();
-    }
+  /**
+   * Discovers the zone name within the uri.
+   * Handles both public zones containing slashes as well as more simple private zone.
+   * Using @PathVariable does not work for public zones.
+   * @param request The incoming http request.
+   * @return The zone name provided within the uri.
+   */
+  private String extractZoneNameFromUri(HttpServletRequest request){
+    // For example, /api/admin//zones/public/region_1
+    String path = (String) request
+        .getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+    // For example, /api/admin/zones/**
+    String bestMatchPattern = (String) request
+        .getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+    // For example, public/region_1
+    return new AntPathMatcher().extractPathWithinPattern(bestMatchPattern, path);
+  }
 
-    @PutMapping("/tenant/{tenantId}/zones/{name}")
-    @ApiOperation(value = "Updates a specific private zone for the tenant")
-    @JsonView(View.Public.class)
-    public ZoneDTO update(@PathVariable String tenantId, @PathVariable String name, @Valid @RequestBody ZoneUpdate zone) {
-        return zoneManagement.updatePrivateZone(tenantId, name, zone).toDTO();
-    }
+  @PostMapping("/tenant/{tenantId}/zones")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiOperation(value = "Creates a new private zone for the tenant")
+  @JsonView(View.Public.class)
+  public ZoneDTO create(@PathVariable String tenantId, @Valid @RequestBody ZoneCreatePrivate zone)
+          throws ZoneAlreadyExists {
+    return zoneManagement.createPrivateZone(tenantId, zone).toDTO();
+  }
 
-    @PutMapping("/admin/zones/{name}")
-    @ApiOperation(value = "Updates a specific public zone")
-    @JsonView(View.Admin.class)
-    public ZoneDTO update(@PathVariable String name, @Valid @RequestBody ZoneUpdate zone) {
-        return zoneManagement.updatePublicZone(name, zone).toDTO();
-    }
+  @PostMapping("/admin/zones")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiOperation(value = "Creates a new public zone")
+  @JsonView(View.Admin.class)
+  public ZoneDTO create(@Valid @RequestBody ZoneCreatePublic zone)
+      throws ZoneAlreadyExists {
+    return zoneManagement.createPublicZone(zone).toDTO();
+  }
 
-    @DeleteMapping("/tenant/{tenantId}/zones/{name}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiOperation(value = "Deletes a specific private zone for the tenant")
-    @JsonView(View.Public.class)
-    public void delete(@PathVariable String tenantId, @PathVariable String name) {
-        zoneManagement.removePrivateZone(tenantId, name);
-    }
+  @PutMapping("/tenant/{tenantId}/zones/{name}")
+  @ApiOperation(value = "Updates a specific private zone for the tenant")
+  @JsonView(View.Public.class)
+  public ZoneDTO update(@PathVariable String tenantId, @PathVariable String name, @Valid @RequestBody ZoneUpdate zone) {
+    return zoneManagement.updatePrivateZone(tenantId, name, zone).toDTO();
+  }
 
-    @DeleteMapping("/admin/zones/{name}")
-    @ApiOperation(value = "Deletes a specific public zone")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @JsonView(View.Admin.class)
-    public void delete(@PathVariable String name) {
-        zoneManagement.removePublicZone(name);
-    }
+  @PutMapping("/admin/zones/{name}")
+  @ApiOperation(value = "Updates a specific public zone")
+  @JsonView(View.Admin.class)
+  public ZoneDTO update(@PathVariable String name, @Valid @RequestBody ZoneUpdate zone) {
+    return zoneManagement.updatePublicZone(name, zone).toDTO();
+  }
 
-    @GetMapping("/tenant/{tenantId}/zones")
-    @ApiOperation(value = "Gets all zones available to be used in the tenant's monitor configurations")
-    @JsonView(View.Public.class)
-    public List<ZoneDTO> getAvailableZones(@PathVariable String tenantId) {
-        return zoneManagement.getAvailableZonesForTenant(tenantId)
-                .stream()
-                .map(Zone::toDTO)
-                .collect(Collectors.toList());
-    }
+  @DeleteMapping("/tenant/{tenantId}/zones/{name}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @ApiOperation(value = "Deletes a specific private zone for the tenant")
+  @JsonView(View.Public.class)
+  public void delete(@PathVariable String tenantId, @PathVariable String name) {
+    zoneManagement.removePrivateZone(tenantId, name);
+  }
 
-    @GetMapping("/tenant/{tenantId}/monitorsByZone/{zone}")
-    @ApiOperation(value = "Gets all monitors in a given zone for a specific tenant")
-    @JsonView(View.Public.class)
-    public List<MonitorDTO> getMonitorsForZone(@PathVariable String tenantId, @PathVariable String zone) {
-        return zoneManagement.getMonitorsForZone(tenantId, zone).stream()
-            .map(Monitor::toDTO)
-            .collect(Collectors.toList());
-    }
+  @DeleteMapping("/admin/zones/{name}")
+  @ApiOperation(value = "Deletes a specific public zone")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @JsonView(View.Admin.class)
+  public void delete(@PathVariable String name) {
+    zoneManagement.removePublicZone(name);
+  }
+
+  @GetMapping("/tenant/{tenantId}/zones")
+  @ApiOperation(value = "Gets all zones available to be used in the tenant's monitor configurations")
+  @JsonView(View.Public.class)
+  public List<ZoneDTO> getAvailableZones(@PathVariable String tenantId) {
+    return zoneManagement.getAvailableZonesForTenant(tenantId)
+        .stream()
+        .map(Zone::toDTO)
+        .collect(Collectors.toList());
+  }
+
+  @GetMapping("/tenant/{tenantId}/monitorsByZone/{zone}")
+  @ApiOperation(value = "Gets all monitors in a given zone for a specific tenant")
+  @JsonView(View.Public.class)
+  public List<MonitorDTO> getMonitorsForZone(@PathVariable String tenantId, @PathVariable String zone) {
+    return zoneManagement.getMonitorsForZone(tenantId, zone).stream()
+        .map(Monitor::toDTO)
+        .collect(Collectors.toList());
+  }
 }
