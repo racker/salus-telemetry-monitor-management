63ba10f2ec58a10cd398eb8364e770eaa0abe209
James Buchan <james.buchan@rackspace.com>
2019-06-04 16:25:09 -0500

    Missing ;

--- src/test/java/com/rackspace/salus/monitor_management/services/MonitorConversionServiceTest.java
+++ src/test/java/com/rackspace/salus/monitor_management/services/MonitorConversionServiceTest.java
@@ -47,7 +47,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID
+import java.util.UUID;
 import javax.validation.ConstraintViolation;
 import org.json.JSONException;
 import org.junit.Assert;
