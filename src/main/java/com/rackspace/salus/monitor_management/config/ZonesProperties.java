65b915bd81bd968c958798b1da4dc0f97cc7f9b1
GeorgeJahad <george.jahad@rackspace.com>
2019-07-22 11:49:00 -0700

    added salus prefix to config parameters (#63)
    * added salus prefix to config parameters
    
    * restore ddl

--- src/main/java/com/rackspace/salus/monitor_management/config/ZonesProperties.java
+++ src/main/java/com/rackspace/salus/monitor_management/config/ZonesProperties.java
@@ -22,7 +22,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties("zones")
+@ConfigurationProperties("salus.zones")
 @Component
 @Data
 public class ZonesProperties {
