2105bb0834df7306c768f8011a678ee190f1f407
James Buchan <james.buchan@rackspace.com>
2019-10-15 17:06:12 -0600

    unused import

--- src/test/java/com/rackspace/salus/monitor_management/services/MonitorManagementTest.java
+++ src/test/java/com/rackspace/salus/monitor_management/services/MonitorManagementTest.java
@@ -34,7 +34,6 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
