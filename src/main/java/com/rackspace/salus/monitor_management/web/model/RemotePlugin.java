da53053f95ac0c6a8f8cbb41bba80b80c2011da0
Geoff Bourne <geoff.bourne@rackspace.com>
2020-02-11 12:09:20 -0600

    Sorted the plugin subtype declarations

--- src/main/java/com/rackspace/salus/monitor_management/web/model/RemotePlugin.java
+++ src/main/java/com/rackspace/salus/monitor_management/web/model/RemotePlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Rackspace US, Inc.
+ * Copyright 2020 Rackspace US, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,20 +26,19 @@ import com.rackspace.salus.monitor_management.web.model.telegraf.MysqlRemote;
 import com.rackspace.salus.monitor_management.web.model.telegraf.NetResponse;
 import com.rackspace.salus.monitor_management.web.model.telegraf.Ping;
 import com.rackspace.salus.monitor_management.web.model.telegraf.PostgresqlRemote;
-import com.rackspace.salus.monitor_management.web.model.telegraf.Redis;
 import com.rackspace.salus.monitor_management.web.model.telegraf.SqlServerRemote;
 import com.rackspace.salus.monitor_management.web.model.telegraf.X509Cert;
 
 @JsonTypeInfo(use = Id.NAME, property = "type")
 @JsonSubTypes({
-    @Type(name = "ping", value = Ping.class),
-    @Type(name = "ssl", value = X509Cert.class),
+    @Type(name = "dns", value = Dns.class),
     @Type(name = "http", value = HttpResponse.class),
-    @Type(name = "net_response", value = NetResponse.class),
     @Type(name = "mysql", value = MysqlRemote.class),
+    @Type(name = "net_response", value = NetResponse.class),
+    @Type(name = "ping", value = Ping.class),
     @Type(name = "postgresql", value = PostgresqlRemote.class),
     @Type(name = "sqlserver", value = SqlServerRemote.class),
-    @Type(name = "dns", value = Dns.class)
+    @Type(name = "ssl", value = X509Cert.class)
 })
 public abstract class RemotePlugin {
 
