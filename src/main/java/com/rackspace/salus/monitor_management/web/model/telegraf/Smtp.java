736b9404f32117473058984213d2241877f3d979
James Buchan <james.buchan@rackspace.com>
2020-03-11 22:17:30 -0600

    Add SMTP remote monitor type (#156)

---
+++ src/main/java/com/rackspace/salus/monitor_management/web/model/telegraf/Smtp.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rackspace.salus.monitor_management.web.model.telegraf;
+
+import com.rackspace.salus.monitor_management.web.model.ApplicableAgentType;
+import com.rackspace.salus.monitor_management.web.model.ApplicableMonitorType;
+import com.rackspace.salus.monitor_management.web.model.RemotePlugin;
+import com.rackspace.salus.monitor_management.web.model.SummaryField;
+import com.rackspace.salus.telemetry.model.AgentType;
+import com.rackspace.salus.telemetry.model.MonitorType;
+import java.time.Duration;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data @EqualsAndHashCode(callSuper = false)
+@ApplicableAgentType(AgentType.TELEGRAF)
+@ApplicableMonitorType(MonitorType.smtp)
+public class Smtp extends RemotePlugin {
+  @SummaryField
+  @NotBlank
+  String host;
+
+  @SummaryField
+  @NotNull
+  @Min(1)
+  @Max(65535)
+  Integer port = 25;
+
+  Duration timeout;
+  Duration readTimeout;
+
+  String ehlo;
+  String from;
+  String to;
+  String body;
+  Boolean starttls;
+
+  String tlsCa;
+  String tlsCert;
+  String tlsKey;
+  Boolean insecureSkipVerify;
+}
