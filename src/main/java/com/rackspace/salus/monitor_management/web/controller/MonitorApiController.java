d14ff59883de179b12d42223391dfc339ab735de
Adam Martinek <harageth@users.noreply.github.com>
2020-07-13 09:50:05 -0700

    Update src/main/java/com/rackspace/salus/monitor_management/web/controller/MonitorApiController.java
    Made search criteria a query parameter
    
    Co-authored-by: Geoff Bourne <geoff.bourne@rackspace.com>

--- src/main/java/com/rackspace/salus/monitor_management/web/controller/MonitorApiController.java
+++ src/main/java/com/rackspace/salus/monitor_management/web/controller/MonitorApiController.java
@@ -332,9 +332,9 @@ public class MonitorApiController {
     return monitorManagement.getTenantMonitorLabelSelectors(tenantId);
   }
 
-  @GetMapping("/tenant/{tenantId}/search/{searchCriteria}")
+  @GetMapping("/tenant/{tenantId}/search")
   @ApiOperation("Finds all monitors that match the searchCriteria either in the monitorName or the ID")
-  public PagedContent<Monitor> getMonitorLabelSelectors(@PathVariable String tenantId, @PathVariable String searchCriteria, Pageable pageable) {
+  public PagedContent<Monitor> getMonitorLabelSelectors(@PathVariable String tenantId, @QueryParam("q") String searchCriteria, Pageable pageable) {
     return PagedContent.fromPage(monitorManagement.monitorSearch(tenantId, searchCriteria, pageable));
   }
 }
