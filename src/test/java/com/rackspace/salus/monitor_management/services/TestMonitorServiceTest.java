5476582fca68f7f4a396057f1ba89fe7f1d4ffbf
shabd67 <abhishek.kumar1@rackspace.com>
2020-09-15 20:28:54 +0530

    used SimpleMeterRegistry instead of MetricsAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class

--- src/test/java/com/rackspace/salus/monitor_management/services/TestMonitorServiceTest.java
+++ src/test/java/com/rackspace/salus/monitor_management/services/TestMonitorServiceTest.java
@@ -73,7 +73,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@Import({SimpleMeterRegistry.class})
 @SpringBootTest
 @Profile("less-logging")
 public class TestMonitorServiceTest {
