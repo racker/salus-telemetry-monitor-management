297fb8519c259c0bfe0234c14097a7a2c2164c79
shabd67 <abhishek.kumar1@rackspace.com>
2020-10-16 23:14:05 +0530

    made corrections for PR comments.

--- src/main/java/com/rackspace/salus/monitor_management/services/MonitorManagement.java
+++ src/main/java/com/rackspace/salus/monitor_management/services/MonitorManagement.java
@@ -1490,19 +1490,11 @@ public class MonitorManagement {
     List<MonitorPolicyDTO> effectivePolicies = policyApi.getEffectiveMonitorPoliciesForTenant(tenantId, false);
 
     // if the old policy had been overriding another one using the same monitorId, find the other one
-    try {
-      List<UUID> newPolicyIds = null;
-      //if policy is in effect and monitor not found
-      try {
-        newPolicyIds = effectivePolicies.stream()
+      List<UUID>  newPolicyIds = effectivePolicies.stream()
+            .filter(p -> event.getMonitorId() != null)
             .filter(p -> p.getMonitorId().equals(monitorId))
             .map(PolicyDTO::getId)
             .collect(Collectors.toList());
-      } catch(NullPointerException e)  {
-        log.debug("Removing cloned policy monitor={} for event={}", clonedMonitor.getId(), event);
-        unbindAndRemoveMonitor(clonedMonitor);
-        return;
-      }
 
       if (newPolicyIds.isEmpty()) {
         log.debug("Removing cloned policy monitor={} for event={}", clonedMonitor.getId(), event);
@@ -1524,13 +1516,6 @@ public class MonitorManagement {
           newPolicyId);
       clonedMonitor.setPolicyId(newPolicyId);
       monitorRepository.save(clonedMonitor);
-    } catch(Exception e)  {
-      log.error("error occurred while processing policy removal event for policy monitor={} and cloned monitor={}",
-          monitorId, clonedMonitor.getId());
-      policyRemvalErrors.tags(MetricTags.OPERATION_METRIC_TAG, "handleMonitorPolicyRemovalEvent","reason",
-          e.getClass().getSimpleName())
-          .register(meterRegistry).increment();
-    }
   }
 
   /**
